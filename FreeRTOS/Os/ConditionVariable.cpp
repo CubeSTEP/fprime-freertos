76f9f8069f084e305c103d02096639d8d973ef96
Tirth-Thakkar <tirth1226@gmail.com>
2026-04-13 23:15:24 -0700

    Adding in Change to Condition Variable As Need Currently for SDK Version

--- FreeRTOS/Os/ConditionVariable.cpp
+++ FreeRTOS/Os/ConditionVariable.cpp
@@ -20,33 +20,28 @@ FreeRTOSConditionVariable::~FreeRTOSConditionVariable() {
 ConditionVariable::Status FreeRTOSConditionVariable::pend(Os::Mutex& mutex) {
     // Release the mutex before waiting for the condition variable
     if (mutex.release() != Os::MutexInterface::Status::OP_OK) {
-        ConditionVariable::Status::ERROR_OTHER;
+        return ConditionVariable::Status::ERROR_OTHER;
     }
-    
+
     // Block the task and wait for the condition variable to be notified
-    BaseType_t result = xSemaphoreTake(m_semaphore, portMAX_DELAY);
+    const BaseType_t result = xSemaphoreTake(m_semaphore, portMAX_DELAY);
     FW_ASSERT(result == pdTRUE);
-    
+
     // Re-acquire the mutex after being notified
     if (mutex.take() != Os::MutexInterface::Status::OP_OK) {
         return ConditionVariable::Status::ERROR_OTHER;
     }
 
-    return ConditionVariable::Status::OP_OK;;
+    return ConditionVariable::Status::OP_OK;
 }
 
 void FreeRTOSConditionVariable::notify() {
-    BaseType_t result = xSemaphoreGive(m_semaphore);
+    const BaseType_t result = xSemaphoreGive(m_semaphore);
+    FW_ASSERT(result == pdTRUE);
 }
 
 void FreeRTOSConditionVariable::notifyAll() {
-    // FreeRTOS doesn't have a direct "notifyAll" equivalent, so we build it
-    // by releasing the semaphore multiple times if tasks are waiting
-    U8 notify_bound = 0;
-    while (uxQueueMessagesWaiting(m_semaphore) > 0 && notify_bound < 100) {
-        BaseType_t result = xSemaphoreGive(m_semaphore);
-        notify_bound++;
-    }
+    this->notify();
 }
 
 ConditionVariableHandle* FreeRTOSConditionVariable::getHandle() {
